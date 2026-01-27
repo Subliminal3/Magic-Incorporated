371788b664add9d4fab368096607ecda1d79db29
Grant Reed <gameact333@gmail.com>
2026-01-27 13:29:55 -0600

    Split two scripts

--- Assets/Scripts/BookText.cs
+++ Assets/Scripts/BookText.cs
@@ -1,42 +1,35 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class BookText : MonoBehaviour
 {
     public TMP_Text bookText;
-    public int maxLength = 3; // max letters in sequence
-    public GameObject crystal;
+    public int maxLength = 3;
 
-    [Header("Sounds")]
-    public AudioClip crystalCrush;
-
-    private bool isCasting = false;
-    private AudioSource aSource;
+    private SpellCastManager spellCastManager; // assign in Inspector
 
     private void Start()
     {
-        // Add or get AudioSource
-        aSource = gameObject.GetComponent<AudioSource>();
-        if (aSource == null)
-            aSource = gameObject.AddComponent<AudioSource>();
+        // Find the SpellCastManager in the scene
+        spellCastManager = FindFirstObjectByType<SpellCastManager>();
+
+        if (spellCastManager == null)
+            Debug.LogError("No SpellCastManager found in the scene!");
     }
 
-    void Update()
+    private void Update()
     {
-        //if right click
-        if (Input.GetMouseButtonDown(1) && !isCasting)
+        //clear text when left-click
+        if (Input.GetMouseButtonDown(0))
         {
-            isCasting = true;
-
-            // Play the crystal sound
-            if (crystalCrush != null)
-                aSource.PlayOneShot(crystalCrush);
-
-            crystal.SetActive(false);
+            bookText.text = "";
         }
 
-        if (!(bookText.text.Length >= maxLength) && isCasting)
+        if (!spellCastManager.IsCasting)
+            return;
+
+        // Add letters while casting
+        if (bookText.text.Length < maxLength)
         {
             if (Input.GetKeyDown(KeyCode.E))
                 bookText.text += "E";
@@ -46,12 +39,10 @@ public class BookText : MonoBehaviour
                 bookText.text += "F";
         }
 
-        if (Input.GetMouseButtonDown(0) && isCasting)
+        //clear text when left-click
+        if (Input.GetMouseButtonDown(0))
         {
             bookText.text = "";
-            isCasting = false;
-
-            crystal.SetActive(true);
         }
     }
 }
--- Assets/Scripts/SpellCastManager.cs
+++ Assets/Scripts/SpellCastManager.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class SpellCastManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Sounds")]
+    public AudioClip crystalCrush;
+    public GameObject crystal;
+
+    private AudioSource aSource;
+    private bool isCasting = false;
+
+    // Lambda property to expose casting state
+    public bool IsCasting => isCasting;
+
+    private void Start()
     {
-        
+        aSource = GetComponent<AudioSource>();
+        if (aSource == null)
+            aSource = gameObject.AddComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Start casting on right click
+        if (Input.GetMouseButtonDown(1) && !isCasting)
+        {
+            isCasting = true;
+
+            if (crystalCrush != null)
+                aSource.PlayOneShot(crystalCrush);
+
+            if (crystal != null)
+                crystal.SetActive(false);
+        }
+
+        // Stop casting on left click
+        if (Input.GetMouseButtonDown(0) && isCasting)
+        {
+            isCasting = false;
+
+            if (crystal != null)
+                crystal.SetActive(true);
+        }
     }
 }
